38fd7f51f20f70cd7accd0766a15f290a71bd793
Xero64 <xero64@gmail.com>
2019-06-23 11:37:50 +0200

    Added setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name="pyvlm",
     version="0.0.1",
     author="Xero64",
-    author_email="xero64@example.com",
+    author_email="xero64@gmail.com",
     description="Vortex Lattice Method in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
