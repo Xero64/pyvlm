1a645ad5efb3caedced28e63a2e66636446faa94
Xero64 <xero64@gmail.com>
2019-09-29 20:44:20 +0200

    Update version to 0.0.4.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyvlm",
-    version="0.0.3",
+    version="0.0.4",
     author="Xero64",
     author_email="xero64@gmail.com",
     description="Vortex Lattice Method in Python",
