470d6eb3eb7e42aca67913a6e3a69ccb92476f98
Xero64 <37905073+Xero64@users.noreply.github.com>
2024-10-28 22:34:46 +0200

    Added symbolic scripts.

---
+++ scripts/sympy/vector_rotation_script.py
@@ -0,0 +1,18 @@
+#%%
+# Import Dependencies
+from sympy import Symbol, cos, sin
+from pygeom.symbol3d import SymbolicVector
+
+#%%
+# Create Symbols
+theta = Symbol('theta', real=True)
+
+v = SymbolicVector(1, 0, 0)
+k = SymbolicVector(0, 0, 1)
+
+vrot: SymbolicVector = v*cos(theta) + k.cross(v)*sin(theta) + k.dot(v)*(1 - cos(theta))*k
+vrot = vrot.simplify()
+print(f'{vrot = }\n')
+
+dvrot = vrot.diff(theta)
+print(f'{dvrot = }\n')
