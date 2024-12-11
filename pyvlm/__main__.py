e2f1b72c7030dbb9489859ed0fe061fe3e23be45
Xero64 <37905073+Xero64@users.noreply.github.com>
2024-12-11 20:11:02 +0200

    Update to __main__.py.

--- pyvlm/__main__.py
+++ pyvlm/__main__.py
@@ -5,7 +5,7 @@ from .classes import latticesystem_from_dict
 from .outputs import latticesystem_to_md, outputs_from_json
 
 
-def main(jsonfilepath: str='', mdfilepath: str=''):
+def main(jsonfilepath: str = '', mdfilepath: str = ''):
 
     if jsonfilepath == '':
         if len(argv) == 1:
